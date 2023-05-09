ea0e5f6b64917d9ee5e70340f1c30dd25fd2de0c
AnakinSkywalker066 <Leegovols@gmail.com>
2023-05-08 22:55:05 -0400

    Initial commit

---
+++ Kenobi.cs
@@ -0,0 +1,17 @@
+﻿using BTD_Mod_Helper;
+using Kenobi;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(Kenobi.Kenobi), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace Kenobi
+{
+   public class Kenobi : BloonsTD6Mod
+   {
+      public override void OnApplicationStart()
+      {
+         ModHelper.Msg<Kenobi>("This Mod Has A Lot Of Custom Files So Be Patient If It Takes A Second To Load... Obi Wan Kenobi Now Has Loaded!");
+      }
+   } 
+}
---
+++ ModHelperData.cs
@@ -0,0 +1,19 @@
+﻿namespace Kenobi;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "36";
+    public const string Version = "1";
+    public const string Name = "Kenobi";
+    public const string Icon = "Icon.png";
+    public const string Description = "High Ground xD";
+
+    public const string RepoOwner = "AnankinSkywalker066"; 
+    public const string RepoName = "Kenobi"; 
+
+
+
+
+
+
+}
